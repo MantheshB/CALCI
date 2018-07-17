7114248eaee346e0556d80a7af5bab224963b1ef
NiTin <NiTin@NiTin-PC>
2018-07-17 09:26:07 +0530

    Contact Manager

---
+++ CONTACT MANAGER/src/com/testersconnect/contacts/Contact.java
@@ -0,0 +1,31 @@
+package com.testersconnect.contacts;
+import java.util.Date;
+
+public class Contact {
+	//Variables
+		String name;
+		String mobile;
+		String email;
+		String twitterId;
+		
+		/*
+		int age;
+		String address;
+		Date dob;
+		String imgLoc;
+		
+		float lat;
+		float lont;
+		
+		*/
+		
+		
+		//Methods
+		
+		
+		
+	}
+
+	
+
+
